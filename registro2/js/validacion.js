84c8968bf826470b70bbcbbca0d08b03f377248f
leonlevrero <triplelevrero@gmail.com>
2022-10-21 10:41:06 -0300

    creo q quedo

--- registro2/js/validacion.js
+++ registro2/js/validacion.js
@@ -9,8 +9,51 @@
     
     function showAlertSuccess() {
         document.getElementById("alert-success").classList.add("show");
+    }
+    
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
+    }
+    var form =  document.querySelectorAll('.needs-validation')
+    
+    function succesCase() {
+        const nombre =  document.getElementById('nombre');
+        const apellido =  document.getElementById('apellido');
+        const email =  document.getElementById('email');
+        const pass1 =  document.getElementById('password1');
+        const pass2 =  document.getElementById('password2');
+        const term =  document.getElementById('terminos');
+        const button = document.getElementById('boton');
+        const form = document.getElementById('form');
+
+        button.addEventListener("click", () => {
+                if (nombre.value != ""){
+                    nombre.classList.add("is-valid");
+                } else {
+                    nombre.classList.add("is-invalid");
+                }
+                if (apellido.value != ""){
+                    apellido.classList.add("is-valid");
+                } else {
+                    apellido.classList.add("is-invalid");
+                }
+                if (email.value != ""){
+                    email.classList.add("is-valid");
+                } else {
+                    email.classList.add("is-invalid");
+                }
+                if (pass1.value != "" && pass1.value.length >= 6){
+                    pass1.classList.add("is-valid");
+                } else {
+                    pass1.classList.add("is-invalid");
+                }
                 if (pass2.value != "" && pass2.value == pass1.value && pass2.value.length >= 6){
+                    pass2.classList.add("is-valid");
+                } else {
                     pass2.classList.add("is-invalid");
+                }
+                if (term.checked ){
+                    term.classList.add("is-valid");
                 } else {
                     term.classList.add("is-invalid");
                 }
