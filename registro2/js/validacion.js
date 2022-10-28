ff486039c4c403625e685d03d8afe40e98ce3759
leonlevrero <triplelevrero@gmail.com>
2022-10-28 09:23:39 -0300

    Falta cambiar el cosito verde y poner el cosito rojo.

--- registro2/js/validacion.js
+++ registro2/js/validacion.js
@@ -14,9 +14,9 @@
     function showAlertError() {
         document.getElementById("alert-danger").classList.add("show");
     }
-    var form =  document.querySelectorAll('.needs-validation')
     
-    function succesCase() {
+    
+   /* function succesCase() {
         const nombre =  document.getElementById('nombre');
         const apellido =  document.getElementById('apellido');
         const email =  document.getElementById('email');
@@ -71,48 +71,66 @@
                 }
             })
         }
-    succesCase();
+    succesCase(); */
 
 
-        /*    if(nombre.value != "" && apellido.value != "" && email.value != "" && 
-            pass1.value != "" && pass2.value != "" && term.checked) {
-                if(pass1.value.length >= 6 && pass2.value == pass1.value){
-                    showAlertSuccess();
-                    form.classList.add("was-validated");
-
-                }
-                else{
-                    
-                    showAlertError(); 
-                }
-            }
-            else{
-                showAlertError();
-            }
-        })
-    }
-    succesCase();
+            
    
 
 
-    // Loop over them and prevent submission validate only if password > 6 and password1 = password2
-   /* Array.prototype.slice.call(forms)
-        .forEach(function (form) {
+    (function () {
+        'use strict'
+      
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var form =  document.querySelectorAll('.needs-validation')      
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(form)
+          .forEach(function (form) {
             form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    alert("Las contraseñas no coinciden o son menores a 6 caracteres");
-                    event.preventDefault()
-                    event.stopPropagation()
-                  
-                }
-
-                form.classList.add('was-validated')
+              if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+              }
+      
+              form.classList.add('was-validated')
             }, false)
-        })
-})() */
+          })
+      })()
 
-  // 
-     
+        
+
+
+  
+     // function to check is password1 = password2 and password1 > 6 characters 
+    function checkPasswordMatch() {
+        let password1 = document.getElementById("password1").value
+        let password2 = document.getElementById("password2").value
+            if (password1.length > 6 ) {
+                document.getElementById("password1").classList.add("is-valid");
+                document.getElementById("password1").classList.remove("is-invalid");  
+            
+            }else {
+                document.getElementById("password1").classList.add("is-invalid");
+                document.getElementById("password1").classList.remove("is-valid");
+            }
+
+              if (password2 === password1) {
+            
+            document.getElementById("password2").classList.add("is-valid");
+            document.getElementById("password2").classList.remove("is-invalid");
+        } else {
+            
+            document.getElementById("password2").classList.add("is-invalid");
+            document.getElementById("password2").classList.remove("is-valid");
+
+        }
+    }
+ 
+    
+
+    
+   
+               
     
     
         
@@ -145,7 +163,7 @@ let boton = document.getElementById("boton")
 boton.addEventListener("click", validateForm())
 
 
-/*
+
 function validateForm() {
     let password1 = document.getElementById("password1").value
     let password2 = document.getElementById("password2").value
@@ -155,5 +173,4 @@ function validateForm() {
         form
     }
     boton.addEventListener("sumbit")
-}
-*/
+} */
